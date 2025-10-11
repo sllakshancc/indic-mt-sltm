b226dd0ad9e44b2bf7d2ce57de6c8dc92cb65396
Thamara Lakshan <sllakshancc@gmail.com>
2025-10-11 12:31:44 +0530

    load exist tokenizer

--- gpe_train.py
+++ gpe_train.py
@@ -176,6 +176,18 @@ class GPETokenizer:
 
 
 
+    def load(self, path):
+        """Load tokenizer from disk."""
+        with open(os.path.join(path, 'gpe_tokenizer.pkl'), 'rb') as f:
+            save_dict = pickle.load(f)
+
+        self.vocab = save_dict['vocab']
+        self.merges = save_dict['merges']
+        self.special_tokens = save_dict['special_tokens']
+        self.vocab_size = save_dict['vocab_size']
+
+        self.vocab_re = {v: k for k, v in self.vocab.items()}
+        self.trained = True
 
     # Helper methods
     def _get_stats(self, ids, counts=None):
